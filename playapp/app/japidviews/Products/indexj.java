25f7ae3b91de492974837af7d2d26795a91cd63d
Bing Ran <bing_ran@hotmail.com>
2011-06-01 00:11:27 +0800

    changed the japid templates to use syntax closer to the new syntax in the latest play-scala module

--- playapp/app/japidviews/Products/indexj.java
+++ playapp/app/japidviews/Products/indexj.java
@@ -1,5 +1,4 @@
 package japidviews.Products;
-
 import java.util.*;
 import java.io.*;
 import cn.bran.japid.tags.Each;
@@ -8,11 +7,13 @@ import static play.templates.JavaExtensions.*;
 import static cn.bran.play.JapidPlayAdapter.*;
 import static play.data.validation.Validation.*;
 import japidviews._layouts.*;
-import static japidviews._javatags.JapidWebUtil.*;
+import play.i18n.Messages;
+import static  japidviews._javatags.JapidWebUtil.*;
 import play.data.validation.Validation;
 import play.mvc.Scope.*;
 import models.*;
 import play.data.validation.Error;
+import play.i18n.Lang;
 import japidviews._tags.*;
 import controllers.*;
 import play.mvc.Http.*;
@@ -21,85 +22,75 @@ import japidviews._javatags.*;
 // NOTE: This file was generated from: japidviews/Products/indexj.html
 // Change to this file will be lost next time the template file is compiled.
 //
+@cn.bran.play.NoEnhance
+public class indexj extends main
+{	public static final String sourceTemplate = "japidviews/Products/indexj.html";
+{
+	headers.put("Content-Type", "text/html; charset=utf-8");
+}
 
-public class indexj extends main {
-	public static final String sourceTemplate = "japidviews/Products/indexj.html";
-	{
-		headers.put("Content-Type", "text/html; charset=utf-8");
-	}
+// - add implicit fields with Play
 
-	// - add implicit fields with Play
-
-	final Request request = Request.current();
-	final Response response = Response.current();
+	final Request request = Request.current(); 
+	final Response response = Response.current(); 
 	final Session session = Session.current();
 	final RenderArgs renderArgs = RenderArgs.current();
 	final Params params = Params.current();
 	final Validation validation = Validation.current();
-	final cn.bran.play.FieldErrors errors = new cn.bran.play.FieldErrors(
-			validation);
-	final play.Play _play = new play.Play();
+	final cn.bran.play.FieldErrors errors = new cn.bran.play.FieldErrors(validation);
+	final play.Play _play = new play.Play(); 
 
-	// - end of implicit fields with Play 
+// - end of implicit fields with Play 
 
-	// -- set up the tag objects
-	final ProductDetail _ProductDetail2 = new ProductDetail(getOut());
-	{
-		_ProductDetail2.setActionRunners(getActionRunners());
-	}
 
-	// -- end of the tag objects
+
+// -- set up the tag objects
+final ProductDetail _ProductDetail0 = new ProductDetail(getOut());
+{ _ProductDetail0.setActionRunners(getActionRunners()); }
+
+// -- end of the tag objects
 
 	public indexj() {
 		super(null);
 	}
-
 	public indexj(StringBuilder out) {
 		super(out);
 	}
-
 	private List<Product> products;
-
 	public cn.bran.japid.template.RenderResult render(List<Product> products) {
 		this.products = products;
 		long t = -1;
-		super.layout();
-		return new cn.bran.japid.template.RenderResultPartial(this.headers,
-				getOut(), t, actionRunners);
+		super.layout("Product listing", "Product listing");
+		return new cn.bran.japid.template.RenderResultPartial(this.headers, getOut(), t, actionRunners);
+	}
+	@Override protected void doLayout() {
+//------
+;// line 1
+;// line 1
+;// line 2
+p("<table>\n" + 
+"    ");// line 3
+for (Product product:products) {// line 6
+p("        <tr>\n" + 
+"            <td>");// line 6
+_ProductDetail0.setOut(getOut()); _ProductDetail0.render(product);
+// line 8
+p("</td>\n" + 
+"            <td>\n" + 
+"                ");// line 8
+for(Category category: product.getCategories()){// line 10
+p("				    ");// line 10
+p(category.getName());// line 11
+p(", \n" + 
+"				");// line 11
+}// line 12
+p("            </td>\n" + 
+"        </tr>\n" + 
+"    ");// line 12
+}// line 15
+p("</table>\n" + 
+"\n");// line 15
+
 	}
 
-	@Override
-	protected void doLayout() {
-		//------
-		p("<table>");
-		for (Product product : products) {
-			p("<tr>\n" +
-					"            <td>");
-			_ProductDetail2.setOut(getOut());
-			_ProductDetail2.render(product);
-
-			p("</td>\n" +
-					"            <td>");
-			for (Category category : product.getCategories()) {
-				p(category.getName());
-				p(",");
-			}
-			p("</td>\n" +
-					"        </tr>");
-		}
-		p("</table>");
-
-	}
-
-	@Override
-	protected void title() {
-		p("Product listing");
-		;
-	}
-
-	@Override
-	protected void pagename() {
-		p("Product listing");
-		;
-	}
 }
--- playapp/app/japidviews/_tags/ProductDetail.java
+++ playapp/app/japidviews/_tags/ProductDetail.java
@@ -1,18 +1,18 @@
 package japidviews._tags;
-
 import java.util.*;
 import java.io.*;
 import cn.bran.japid.tags.Each;
-import s.*;
 import static play.templates.JavaExtensions.*;
 import static cn.bran.play.JapidPlayAdapter.*;
 import static play.data.validation.Validation.*;
 import japidviews._layouts.*;
-import static japidviews._javatags.JapidWebUtil.*;
+import play.i18n.Messages;
+import static  japidviews._javatags.JapidWebUtil.*;
 import play.data.validation.Validation;
 import play.mvc.Scope.*;
 import models.*;
 import play.data.validation.Error;
+import play.i18n.Lang;
 import japidviews._tags.*;
 import controllers.*;
 import play.mvc.Http.*;
@@ -21,58 +21,55 @@ import japidviews._javatags.*;
 // NOTE: This file was generated from: japidviews/_tags/ProductDetail.html
 // Change to this file will be lost next time the template file is compiled.
 //
+@cn.bran.play.NoEnhance
+public class ProductDetail extends cn.bran.japid.template.JapidTemplateBase
+{	public static final String sourceTemplate = "japidviews/_tags/ProductDetail.html";
+{
+	headers.put("Content-Type", "text/html; charset=utf-8");
+}
 
-public class ProductDetail extends cn.bran.japid.template.JapidTemplateBase {
-	public static final String sourceTemplate = "japidviews/_tags/ProductDetail.html";
-	{
-		headers.put("Content-Type", "text/html; charset=utf-8");
-	}
+// - add implicit fields with Play
 
-	// - add implicit fields with Play
-
-	final Request request = Request.current();
-	final Response response = Response.current();
+	final Request request = Request.current(); 
+	final Response response = Response.current(); 
 	final Session session = Session.current();
 	final RenderArgs renderArgs = RenderArgs.current();
 	final Params params = Params.current();
 	final Validation validation = Validation.current();
-	final cn.bran.play.FieldErrors errors = new cn.bran.play.FieldErrors(
-			validation);
-	final play.Play _play = new play.Play();
+	final cn.bran.play.FieldErrors errors = new cn.bran.play.FieldErrors(validation);
+	final play.Play _play = new play.Play(); 
 
-	// - end of implicit fields with Play 
+// - end of implicit fields with Play 
+
 
 	public ProductDetail() {
 		super(null);
 	}
-
 	public ProductDetail(StringBuilder out) {
 		super(out);
 	}
-
-	private Product product;
-
-	public cn.bran.japid.template.RenderResult render(Product product) {
+	private s.Product product;
+	public cn.bran.japid.template.RenderResult render(s.Product product) {
 		this.product = product;
 		long t = -1;
 		super.layout();
-		return new cn.bran.japid.template.RenderResultPartial(this.headers,
-				getOut(), t, actionRunners);
+		return new cn.bran.japid.template.RenderResultPartial(this.headers, getOut(), t, actionRunners);
 	}
-
-	@Override
-	protected void doLayout() {
-		//------
-		p("<div class=\"product\">\n" +
-				"	<img src=\"");// line 3
-		p(product.getName());// line 5
-		p(".jpg\" />\n" +
-				"	<span class=\"productname\">");// line 5
-		p(product.getName());// line 6
-		p("</span>, <span class=\"price\">$");// line 6
-		p(product.getPrice());// line 6
-		p("</span>\n" +
-				"</div>\n");// line 6
+	@Override protected void doLayout() {
+//------
+;// line 1
+p("\n" + 
+"\n" + 
+"<div class=\"product\">\n" + 
+"	<img src=\"");// line 1
+p(product.getName());// line 4
+p(".jpg\" />\n" + 
+"	<span class=\"productname\">");// line 4
+p(product.getName());// line 5
+p("</span>, <span class=\"price\">$");// line 5
+p(product.getPrice());// line 5
+p("</span>\n" + 
+"</div>\n");// line 5
 
 	}
 
